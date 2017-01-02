ffcbfe05c525bc465406deb032ed673ae8bc71db
dror-mor <drori.mor@gmail.com>
2017-01-02 11:16:24 +0200

    updates vampires.rb, age and garlic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,3 +11,15 @@ wants_garlic = gets.chomp
 
 puts "Would you like to enroll in the company’s health insurance?"
 wants_insurance = gets.chomp
+
+got_age_right = false
+likes_garlic = true
+
+if 2017-employee_age.to_i == employee_birth_year.to_i
+	got_age_right = true
+end
+
+if wants_garlic == "n" || wants_garlic == "no"
+	likes_garlic = false
+end
+
