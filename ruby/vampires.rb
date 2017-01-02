66067c07dd11ac1744087bc627f5e07b7dcb80ff
dror-mor <drori.mor@gmail.com>
2017-01-02 10:50:49 +0200

    updates vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "How old are you? What year were you born?"
+employee_age = gets.chomp
+employee_birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+wants_garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+wants_insurance = gets.chomp
