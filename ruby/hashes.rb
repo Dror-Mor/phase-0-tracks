a20872cd652a1f6adff44c09f608992abfd8189f
dror-mor <drori.mor@gmail.com>
2017-01-08 18:13:08 +0200

    updated work

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,7 +1,7 @@
-# Create cliant hash with the following keys: Name, Age, Number of children, Decor theme, Number of rooms, Favorite color, eco friendly.
+# Create cliant hash with the following keys: Name, Age, Number of children, Decor theme, Number of rooms, eco friendly.
 # ask user to fill in the keys
 # convert keys to thir types: Name - str, Age - int, Number of children - int,
-# Decor theme - str, Number of rooms - int, Favorite color - str, eco friendly - bool.
+# Decor theme - str, Number of rooms - int, eco friendly - bool.
 # Print current answers
 # ask if there are any changes
 # if there are - allow user to change the wanted key and update it, if there aren't print final version
@@ -11,8 +11,7 @@ cliant = {
 }
 
 puts "Please enter cliants name:"
-user_input = gets.chomp
-cliant[:name] = caliant_name
+cliant[:name] = gets.chomp
 
 puts "Please enter cliants age:"
 user_input = gets.chomp
@@ -20,5 +19,35 @@ cliant[:age] = user_input.to_i
 
 puts "Please enter number of children:"
 user_input = gets.chomp
-cliant[:num_of_children] = user_input.to_i
+cliant[:children] = user_input.to_i
 
+puts "Please enter decor theme:"
+cliant[:decore] = gets.chomp
+
+puts "Please enter number of rooms:"
+user_input = gets.chomp
+cliant[:rooms] = user_input.to_i
+
+puts "Would you like the design to be eco friendly? (y/n)"
+user_input = gets.chomp
+if user_input == "y"
+	cliant[:eco] = true
+else
+	cliant[:eco] = false
+end
+
+puts "Please review the following information:"
+p cliant
+
+puts "Would you like the change any of the following? if so, please type field name. if not, please type `none`"
+user_input = gets.chomp
+case user_input
+	when "name"
+		puts "Please enter new name:"
+		cliant[:name] = gets.chomp
+	when "age"
+		puts "Please enter new age:"
+		user_input = gets.chomp
+		cliant[:age] = user_input.to_i
+
+	
