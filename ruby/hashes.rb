9fa88101cb036e9d0f7eaf5d8f08bfffea53e1ea
dror-mor <drori.mor@gmail.com>
2017-01-08 17:53:53 +0200

    starts hashes.rb file

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+# Create cliant hash with the following keys: Name, Age, Number of children, Decor theme, Number of rooms, Favorite color, eco friendly.
+# ask user to fill in the keys
+# convert keys to thir types: Name - str, Age - int, Number of children - int,
+# Decor theme - str, Number of rooms - int, Favorite color - str, eco friendly - bool.
+# Print current answers
+# ask if there are any changes
+# if there are - allow user to change the wanted key and update it, if there aren't print final version
+
+cliant = {
+
+}
+
+puts "Please enter cliants name:"
+user_input = gets.chomp
+cliant[:name] = caliant_name
+
+puts "Please enter cliants age:"
+user_input = gets.chomp
+cliant[:age] = user_input.to_i
+
+puts "Please enter number of children:"
+user_input = gets.chomp
+cliant[:num_of_children] = user_input.to_i
+
