b88b39dc81b69806485c03d596c5bbb87def2c6c
dror-mor <drori.mor@gmail.com>
2017-01-10 15:04:29 +0200

    initializes nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,18 @@
+store = {
+	mens: {
+		featured_season: 'Summer',
+		racks: {
+			hanging: 2,
+			shelves: 5
+		} 
+		sellers: ["Michelle", "Omar"]
+	}
+	womens: {
+		featured_season: 'Spring',
+		racks: {
+			hanging: 2,
+			shelves: 5
+		} 
+		sellers: ["Michelle", "Omar"]
+	}
+}
