e314734ea47838275b3cdc8d6fdad6125349b0b0
dror-mor <drori.mor@gmail.com>
2017-01-10 15:23:18 +0200

    updates nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,15 +4,43 @@ store = {
 		racks: {
 			hanging: 2,
 			shelves: 5
-		} 
-		sellers: ["Michelle", "Omar"]
-	}
+		},
+		sellers: [
+			"Michelle", 
+			"Omar"
+			],
+		items_on_sale: ["Hats"]
+	},
 	womens: {
 		featured_season: 'Spring',
 		racks: {
-			hanging: 2,
-			shelves: 5
-		} 
-		sellers: ["Michelle", "Omar"]
+			hanging: 7,
+			shelves: 10
+		},
+		sellers: [
+			"Lea",
+			"Maggie",
+			"Rachel",
+			"Steven"
+			],
+		items_on_sale: ["Golden belt", "Red dress" ]
+	},
+	kids: {
+		featured_season: 'Spring',
+		racks: {
+			hanging: 3,
+			shelves: 3
+		},
+		sellers: ["Mariah"],
+		items_on_sale: []
 	}
 }
+
+# How many shelves are there in the womens department?
+puts "\nThere are #{store[:womens][:racks][:shelves]} shelves at the womens department."
+
+# Who is working at the mens department?
+puts "\nHere is a list for emloyees at the mens department:"
+store[:mens][:sellers].each do |names|
+	puts names
+end
