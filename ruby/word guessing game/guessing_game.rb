5212ba9e2e7dca261bf3fb57f31aca54b6594b27
dror-mor <drori.mor@gmail.com>
2017-01-18 18:50:27 +0200

    better version of init

--- ruby/word guessing game/guessing_game.rb
+++ ruby/word guessing game/guessing_game.rb
@@ -1,13 +1,29 @@
 class WordGuessing
-	attr_accessor :word_to_guess :max_num_guesses :guessed_so_far
-	attr_reader :game_over
+	attr_accessor :num_of_guesses, :word_so_far
+	attr_reader :word_to_guess
 
-	def initialize
-		@word_to_guess = []
-		@max_num_guesses = nil
-		@guessed_so_far = []
+	def initialize(word)
+		@word_to_guess = word.downcase.split('')
+		@num_of_guesses = word.length * 2
+		@word_so_far = []
 		@game_over = false
+		@win_game = false
 	end
 
-game = WordGuessing.new
-puts "Please enter word to guess:"
+	def status
+		puts "\nThis is what you have so far:\n"
+		for i in 0..word_to_guess.length
+			if word_to_guess[i] == nil
+				puts "_ "
+			else
+				puts "#{word_to_guess[i] }"
+			end
+		end
+	end
+end
+
+puts "Please enter a word you want the other user to guess:"
+user_word = gets.chomp
+game = WordGuessing.new(user_word)
+
+p game.word_to_guess
