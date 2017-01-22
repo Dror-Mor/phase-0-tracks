47df51ea52c8bf89c2b02981ad9c795d8f53a76e
dror-mor <drori.mor@gmail.com>
2017-01-22 23:00:27 +0200

    updates guessing game to fit the test file, sonding the driver code into its own method within the class, adding rspec tests to all methods

--- ruby/word guessing game/guessing_game.rb
+++ ruby/word guessing game/guessing_game.rb
@@ -6,7 +6,8 @@
 # def “status” that will show the second users progress:
 # 	- if word to guess at an index is empty (nil), print “_ “
 # 	- else print the letter in that possition.
-# 	- also, if the user completed the entire word, letter by letter, end the game and 	congratulate them.
+# def "check complete" will go over the letters found so far and end the game if completed.
+# 	- if the user completed the entire word, letter by letter, end the game and congratulate them.
 # def “check guess” will get a word or letter that the user wants to try and the the word that needs to be guessed.
 # 	- if user enters the entire word and guesses correctly - congratulate them and end the game.
 # 	- else if the user gives a letter or word that they already tried before - print out 
@@ -19,7 +20,7 @@
 # make sure driver code iterates until game is over and calls status with every loop.
 
 class WordGuessing
-	attr_accessor :num_of_guesses, :word_so_far, :past_guesses, :user_word
+	attr_accessor :num_of_guesses, :word_so_far, :past_guesses, :user_word, :word
 	attr_reader :word_to_guess, :game_over
 
 	def initialize(word)
@@ -28,14 +29,15 @@ class WordGuessing
 		@word_so_far = []
 		@past_guesses = []
 		@game_over = false
+		@word = word
 	end
 
-	def check_guess(user_guess, user_word)
-		if user_guess.downcase == user_word.downcase
+	def check_guess(user_guess)
+		if user_guess.downcase == @word.downcase
 			@game_over = true
-			puts "Good job! the word is #{user_word}"
+			p "Good job! the word is #{@word}"
 		elsif @past_guesses.include?(user_guess.downcase)
-			puts "You already tired guessing '#{user_guess}'"
+			p "You already tried guessing '#{user_guess}'"
 		else
 			result = @word_to_guess.each_index.select{ |i| @word_to_guess[i].downcase == user_guess.downcase }
 			if result.empty?
@@ -43,7 +45,7 @@ class WordGuessing
 				@past_guesses << user_guess.downcase
 				if @num_of_guesses == 0
 					@game_over = true
-					puts "You're out of guesses. the word was #{user_word}"
+					p "You're out of guesses. the word was #{@word}"
 				end
 			else
 				result.each { |i| @word_so_far[i] = user_guess }
@@ -51,36 +53,44 @@ class WordGuessing
 		end
 	end
 
-	def status
-		puts "\nYou have #{@num_of_guesses} guesses left\nAnd this is what you have so far:\n"
+	def check_complete
 		is_complete = true
+		@word_to_guess.each_index do |i|
+			is_complete = !@word_so_far[i].nil? if is_complete
+		end
+		if is_complete
+			@game_over = true
+			p "Good job! the word is #{@word}" 
+		end
+	end
+
+	def status
+		p "You have #{@num_of_guesses} guesses left\nAnd this is what you have so far:\n"
 		@word_to_guess.each_index do |i|
 			if @word_so_far[i].nil?
 				print "_ "
-				is_complete = false
 			else
-				print @word_so_far[i] #I think we need to add a condition to end the loop here
+				print @word_so_far[i]
 			end
 		end
-		if is_complete
-			puts "Good job! the word is #{user_word}" #how do I make user_word available here?
-			@game_over = true
+	end
+
+	def play
+		puts "Please enter a word you want the other user to guess:"
+		user_word = gets.chomp
+
+		system "clear"
+		puts "The user before you gave us a word. let's see if you can guess it!"
+
+		while !@game_over
+			status
+			puts "\nPlease ask for a letter or try to guess the entire word:"
+			user_guess = gets.chomp
+			check_guess(user_guess, user_word)
+			check_complete
 		end
 	end
 end
 
-puts "Please enter a word you want the other user to guess:"
-user_word = gets.chomp
-game = WordGuessing.new(user_word)
-
-system "clear"
-puts "The user before you gave us a word. let's see if you can guess it!"
-
-while !game.game_over
-	game.status
-	break if game.game_over
-	puts "\nPlease ask for a letter or try to guess the entire word:"
-	user_guess = gets.chomp
-	game.check_guess(user_guess, user_word)
-end
-
+# game = WordGuessing.new(user_word)
+# game.play
--- ruby/word guessing game/guessing_game_spec.rb
+++ ruby/word guessing game/guessing_game_spec.rb
@@ -1,7 +1,54 @@
-require_relative 'WordGuessing'
+require_relative 'guessing_game'
 
 describe WordGuessing do
-	let(:wordguessing) { WordGuessing.new}
+	let(:wordguessing) { WordGuessing.new("Hello") }
+#check guess
+	it 'Ends game if the user guessed the entire word' do
+		expect(wordguessing.check_guess("Hello")).to eq "Good job! the word is Hello"
+	end
 
-	it "Checks if the letter is in the word to guess" do
-		expect(wordguessing.check_guess(hello, h)).to 
+	it 'Continues game if user didnt guess the entire word' do
+		wordguessing.check_guess("bye")
+		expect(wordguessing.game_over).to eq false
+	end
+
+	it 'Adds the right letter to the word array if the user guessed right' do
+		wordguessing.check_guess("H")
+		expect(wordguessing.word_so_far[0]).to eq "H"	
+	end
+
+	it 'Doesnt add the letter if it isnt in the word' do
+		wordguessing.check_guess("g")
+		expect(wordguessing.word_so_far[0]).to eq nil
+	end
+
+	it 'Decreases num of guesses with every wrong guess' do
+		wordguessing.check_guess("g")
+		expect(wordguessing.num_of_guesses).to eq 9
+	end
+
+	it 'Ends game if user reached 0 guesses' do
+		game = WordGuessing.new("H")
+		game.check_guess("a")
+		game.check_guess("g")
+		expect(game.game_over).to eq true
+	end
+
+	it 'Prints if the user already guessed same letter' do
+		wordguessing.check_guess("a")
+		expect(wordguessing.check_guess("a")).to eq "You already tried guessing 'a'"
+
+	end
+#check complete
+	it 'Checks if the user completed the word' do
+		wordguessing.check_guess("h")
+		wordguessing.check_guess("e")
+		wordguessing.check_guess("l")
+		wordguessing.check_guess("o")
+		expect(wordguessing.check_complete).to eq "Good job! the word is Hello"
+		
+	end
+# status
+	it 'Prints the letters found so far' do
+	end
+end
