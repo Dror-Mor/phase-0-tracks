a9f4ffc432e75e2cc1461947dcf7bc6e4e2a6154
dror-mor <drori.mor@gmail.com>
2017-01-26 15:50:19 +0200

    not yet

--- js/algos.js
+++ js/algos.js
@@ -10,6 +10,10 @@
 // Create a condition to check if one of object key-values is like the other
 // if so - return true, if not, return false.
 
+//Release 2
+//Create a method that takes an number and produces as many strings.
+//the number will tell it how many times to itterate. 
+//within this iteration will be another one that will add chars to everyword
 function longest_item(arr) {
 	max_length = 0;
 	max_index = 0;
@@ -20,33 +24,52 @@ function longest_item(arr) {
 			max_index = i;
 		}
 	}
+	console.log("The longest item is: "+ "'"+arr[max_index]+"'");
 	return max_index;
 }
 
 function same_value(object_a, object_b){
 	if (object_a.name == object_b.name || object_a.age == object_b.age){
+		console.log("True")
 		return true;}
 	else{
+		console.log("False")
 		return false;
 	}
 }
+
+function array_generator(int){
+	var arr = [];
+	var alphabet = "abcdefghijklmnopqrstuvwxyz"
+	for (var i = 0; i < int; i++) {
+		var word = "" 
+		for (var j = 0; j < Math.floor(Math.random() * 11)+1; j++) {
+			word += alphabet[Math.floor(Math.random() * alphabet.length)];
+		}	
+		arr.push(word)
+	}
+	return arr;
+}
+
 var items = ["this is long", "i think this is the longest one", "this is also long"];
-max_index = longest_item(items);
-console.log("The longest item is: "+ "'"+items[max_index]+"'");
+longest_item(items);
 
 var items = ["Apple", "Ring", "Dresser", "Computers", "Rope"];
-max_index = longest_item(items);
-console.log("The longest item is: "+ "'"+items[max_index]+"'");
+longest_item(items);
+
 
 var items = ["JavaScript", "Ruby", "C++", "C#", "Python"];
-max_index = longest_item(items);
-console.log("The longest item is: "+ "'"+items[max_index]+"'");
+longest_item(items);
+
 
 var a = {name: "Ron", age: 27};
 var b = {name: "Thomas", age: 27};
 var c = {name: "Ellie", age: 34};
 var d = {name: "Ellie", age: 36};
 
-console.log(same_value(a,b))
-console.log(same_value(c,d))
-console.log(same_value(a,d))
+same_value(a,b)
+same_value(c,d)
+same_value(a,d)
+
+arr = array_generator(20)
+longest_item(arr)
