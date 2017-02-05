7ec5c48814628854caf0bae43da5eea57fbe488a
dror-mor <drori.mor@gmail.com>
2017-02-05 22:52:08 +0200

    adds drop method and menu

--- calories/calories.rb
+++ calories/calories.rb
@@ -4,7 +4,9 @@ db = SQLite3::Database.new("daily_cal.db")
 db.results_as_hash = true
 
 continue = true
+$table_exists = false
 
+# allow user to add an item to the list.
 def add_item(db)
 	create_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS items (
@@ -12,50 +14,71 @@ def add_item(db)
 		name VARCHAR(255),
 		calories INT
 	)
-SQL
-
+	SQL
 	db.execute(create_table_cmd)
+
+	$table_exists = true
+
 	puts "Please enter item name:"
 	item_name = gets.chomp
 	puts "Please enter calories for #{item_name}:"
-	cals = gets.chomp.to_int
+	cals = gets.chomp.to_i
 	db.execute("INSERT INTO items (name, calories) VALUES (? ,?)", [item_name, cals])
 	puts "Item added successfully!"
 end
 
-#add_item(db, "Bread", 200)
-
-
+# allow user to see all the items in the list and the sum of calories consumed. 
 def summary(db)
-	all_items = db.execute("SELECT * FROM items")
-	total = 0
-	all_items.each do |item|
-		puts "#{item['id']}) #{item['name']} - #{item['calories']} calories"
-		total += item['calories']
+	if $table_exists
+		all_items = db.execute("SELECT * FROM items")
+		total = 0
+		all_items.each do |item|
+			puts "\n#{item['id']}) #{item['name']} - #{item['calories']} calories"
+			total += item['calories']
+		end
+		puts "You have consumed a total of #{total} calories so far"
+	else
+		puts "There are no items in your table."
 	end
-	puts "You have consumed a total of #{total} calories so far"
+end
+
+# allow user to delete table.
+def drop(db)
+	if $table_exists
+		db.execute("DROP TABLE items;")
+		$table_exists = false
+		puts "\nTable successfully deleted."
+	else
+		puts "\nTable successfully deleted."
+	end
 end
 
 "Hello, and welcome to Calorie Counter!"
-#summary(db)
-while continue = true
-	puts "Please enter the number of wanted option:\n
-	1. Add item\n
-	2. See summary\n
-	3. Delete table\n
-	4. Exit\n"
+
+while continue == true
+	puts "\nWhat would you like to do next? Please enter the number of wanted option:
+	1. Add item
+	2. See summary
+	3. Delete table
+	4. Exit"
 
 	user_choice = gets.chomp
 
 	case user_choice
-		case "1"
+		when "1"
 			add_item(db)
 
-		case "2"
+		when "2"
 			summary(db)
+
+		when "3"
+			drop(db)
+# allow user to exit.
+		when "4"
+			puts "Thank you. See you soon!"
+			continue = false
 	end
 end
-# allow user to add an item to the list.
-# allow user to see all the items in the list and the sum of calories consumed. 
-# allow user to delete table.
-# allow user to exit.
+
+
+
